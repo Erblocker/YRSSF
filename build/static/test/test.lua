f54077875ccc949cbba130ea30fdd886dc078f31
cgoxopx <xhackerxxxx@sina.com>
2017-09-23 16:35:44 +0000

    紧急更新，解决64位兼容性问题

--- build/static/test/test.lua
+++ build/static/test/test.lua
@@ -1,18 +1,21 @@
-web={
-  ["get"]="",
-  ["post"]="",
-  ["cookie"]="",
-  ["header"]=""
+local web={
+  ["get"]={},
+  ["post"]={},
+  ["cookie"]={},
+  ["header"]={}
 }
 Httpd.init(Request.paser)
 web.header=Httpd.getheaderArray(Request.paser)
+print("get header")
 web.cookie=Httpd.getcookieArray(Request.paser)
+print("get cookie")
 web.get=Httpd.getqueryArray(Request.paser)
-Httpd.readBufferBeforeSend(Request.fd)
+print("get query")
+---- Httpd.readBufferBeforeSend(Request.fd)
 Httpd.write(Request.fd,
   "HTTP/1.0 200 OK\r\n"..
   "Content-Type:text/xml;charset=UTF-8\r\n"..
   "Cache-Control:no-cache\r\n"..
   "\r\n")
 Httpd.write(Request.fd,"hello\r\n")
-Httpd.write(Request.fd,web.get["id"])
+Httpd.write(Request.fd,cjson.encode(web))
